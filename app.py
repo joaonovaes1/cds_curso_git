302225fbdf8d5dea8849d00b80b2fa1c4d19f213
João Marcelo Novaes <joaomnovaes1998@gmail.com>
2023-05-14 12:28:00 -0300

    added main function

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ import numpy as np
 import streamlit as st
 
 def load_data():
-    
+
     return pd.read_csv('data/processed/bikes_completed.csv')
 
-df = load_data()
 
-st.dataframe(df)
+def main ():
+    df = load_data()
+
+    st.dataframe(df)
+
+
+if __name__ == '__main__':
+    main()
+
+
